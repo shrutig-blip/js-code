8629c35f2aaef08d4452ccb39731934ae49bfb42
Shruti Gupta <shrutigupta6002@gmail.com>
2026-07-21 14:38:35 +0000

    operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,28 +1,56 @@
-//for multiple comment out-> select->ctrl+/
-let score = "99sd";
-console.log(typeof score); 
-console.log(typeof (score));
+// //for multiple comment out-> select->ctrl+/
+// let score = "99sd";
+// console.log(typeof score); 
+// console.log(typeof (score));
 
-let valueInNumber= Number(score);
-console.log(typeof valueInNumber); //number
-console.log( valueInNumber); //NaN
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log( valueInNumber); //NaN
 
-//NaN for mixed,strings and undefined value, typeof is still number after the conversion but number itself becomes NaN
-//1 for true
-//0 for null and false
+// //NaN for mixed,strings and undefined value, typeof is still number after the conversion but number itself becomes NaN
+// //1 for true
+// //0 for null and false
 
 
-let isLoggedIn =1;
-let a="";
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-let booleanA= Boolean(a);
-console.log(typeof booleanIsLoggedIn); //boolean
-console.log(booleanIsLoggedIn) //true
-console.log(booleanA) //false
+// let isLoggedIn =1;
+// let a="";
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// let booleanA= Boolean(a);
+// console.log(typeof booleanIsLoggedIn); //boolean
+// console.log(booleanIsLoggedIn) //true
+// console.log(booleanA) //false
 
-//false for empty String, otherwise true
+// //false for empty String, otherwise true
 
-let num=65;
-let stringnum= String(num);
-console.log(typeof stringnum);
-console.log(stringnum)
+// let num=65;
+// let stringnum= String(num);
+// console.log(typeof stringnum);
+// console.log(stringnum)
+
+
+
+
+//*********************operations****************************************
+
+let val=3;
+let neg=-val;
+// + , - , * , ** , / , % , ++
+
+let str1="hello "
+let str2="shruti"
+let str3=str1+str2
+console.log(str3); 
+
+//ecms script guidelines
+console.log(1+"2"); //12
+console.log("1"+2); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+
+console.log(true); //true
+console.log(+true); //1  (+ is a way of conversion)
+//console.log(true+); //error
+console.log(+""); //0
+
+let num1,num2,num3;
+
